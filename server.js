aea64915223ff39fc20e9c350eb2e835b323b7a7
Nguyen Duc <ducnm.b21cn249@stu.ptit.edu.vn>
2024-10-17 11:10:06 +0700

    update ip

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require('./src/app.js');
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+// Lắng nghe trên tất cả địa chỉ IP với '0.0.0.0'
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
